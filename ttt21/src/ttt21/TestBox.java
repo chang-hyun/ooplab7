1552848c836378d57e8a0489d53699eeda9d6ff1
황재진 <@192.168.0.2>
2017-06-10 15:28:59 +0900

    123

---
+++ ttt21/src/ttt21/TestBox.java
@@ -0,0 +1,31 @@
+package ttt21;
+
+public class TestBox {
+	
+
+	public static void main(String[] args)
+	{
+		TestBox t = new TestBox();
+		t.go();
+	}
+	
+	public void go()
+	{
+		int foo = 4;
+		String bar = "Hello";
+		
+		takeStringAndInt(""+foo,foo);
+	}
+	
+	void takeStringAndInt(String s,int x)
+	{
+		System.out.println("we got "+s+" and "+x);
+	}
+}
+
+abstract class Person
+{
+	public abstract void storage();
+	
+}
+
